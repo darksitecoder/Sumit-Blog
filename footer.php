a894eaca27a871a7c65c0f17ca6c27ed41e776ff
Sumit Pal <50999497+darksitecoder@users.noreply.github.com>
2022-01-18 19:04:23 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="footer text-left d-flex py-2 theme-bg-dark px-3">
+		   
+           <p class="copyright"><a href="https://darksitecoder1.blogspot.com">Darksitecoder</a></p>
+          <div class="container px-1  my-0 col-sm-5">
+          <?php
+          dynamic_sidebar('footer-1');
+          ?>
+          </div>
+       </footer>
+   
+   </div>
+
+
+<?php
+	
+	wp_footer();
+	
+	?>
+    
+
+</body>
+</html> 
+
